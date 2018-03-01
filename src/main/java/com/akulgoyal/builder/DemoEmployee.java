0050e3d8c5184df2041d362a55870f8992a721cc
akulgoyal <akul@theinnovationinc.co>
2018-03-01 10:43:40 +0530

    Added builder pattern

---
+++ src/main/java/com/akulgoyal/builder/DemoEmployee.java
@@ -0,0 +1,21 @@
+package com.akulgoyal.builder;
+
+import java.util.UUID;
+
+public class DemoEmployee {
+
+    public static void main(String args[]) {
+
+        Employee employee = new Employee.Builder()
+                .setId(UUID.randomUUID().toString())
+                .setDept("IT")
+                .setName("Akul")
+                .setPhone("+91-9999999999")
+                .build();
+
+        System.out.println("Employee id: " + employee.getId());
+        System.out.println("Employee name: " + employee.getName());
+        System.out.println("Employee works in " + employee.getDept() + " department");
+        System.out.println("Employee's mobile number: " + employee.getPhone());
+    }
+}
---
+++ src/main/java/com/akulgoyal/builder/Employee.java
@@ -0,0 +1,66 @@
+package com.akulgoyal.builder;
+
+public class Employee {
+
+    private final String id;
+    private final String name;
+    private final String dept;
+    private final String phone;
+
+    private Employee(String id, String name, String dept, String phone) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    static class Builder {
+        private String id;
+        private String name;
+        private String dept;
+        private String phone;
+
+        public Builder() {
+        }
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDept(String dept) {
+            this.dept = dept;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Employee build() {
+            return new Employee(id, name, dept, phone);
+        }
+    }
+}
